15afc40a5b9878d4aac93446cfeb852f5f264d95
Fernando <fernando.santos@aluno.imepac.edu.br>
2025-10-10 17:18:31 -0300

    segundo commit

---
+++ clinic-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Patients from './pages/Patients';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/patients" element={<Patients />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
